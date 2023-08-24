0333743c201b7d9553e27d503fa21a3a20c90c94
KaterynaChaplynska <101507992+KaterynaChaplynska@users.noreply.github.com>
2023-08-24 23:29:31 +0300

    new key

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import Notiflix from "notiflix";
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1/'; //! Разобраться!
-axios.defaults.headers.common["x-api-key"] = "live_O09bKfH8yahzZLzndnmmAl65FxhRF90HGqtjREcQdbsBsS4fyO2MF7pFJtwY7Otf";
+axios.defaults.headers.common["x-api-key"] = "live_rnU55rCXqKPD83Egmapcdap90VKAblzQGTKZl8H3JKOckJcU60JEOZSi2DteHMrS";
 
 
-export {fetchBreeds};
-export {fetchCatByBreed};
+export { fetchBreeds };
+export { fetchCatByBreed };
 
 function fetchBreeds() {
 
@@ -18,14 +18,14 @@ function fetchBreeds() {
 
 
 function fetchCatByBreed(breedId) {
-  
+
   return axios.get(`images/search?breed_ids=${breedId}`)
-  .then(response => {
+    .then(response => {
 
-    return response.data;
-  })
-  .catch(error => {
+      return response.data;
+    })
+    .catch(error => {
 
-    Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!");
-  });
+      Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!");
+    });
 }
